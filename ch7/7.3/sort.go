84e0e590a6575d56aa3bdf821d0eee34f3e10946
Looper <newlooper@hotmail.com>
2020-09-13 16:33:14 +0800

    Ex. 7.3

---
+++ ch7/7.3/sort.go
@@ -0,0 +1,63 @@
+package treesort
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+func (node *tree) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+
+	for _, v := range appendValues(nil, node) {
+		buf.WriteString(strconv.Itoa(v))
+		buf.WriteByte(' ')
+	}
+
+	if buf.Len() > len("{") {
+		buf.Truncate(buf.Len() - 1)
+	}
+
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice.
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}.
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
